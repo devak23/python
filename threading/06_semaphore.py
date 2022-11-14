4b78e9c7a859839dc92abeaa992b304aca38eb3a
Abhay Kulkarni <developer.ak23@gmail.com>
2022-11-14 18:11:38 +0530

    Semaphore examples

--- threading/06_semaphore.py
+++ threading/06_semaphore.py
@@ -1,83 +1,90 @@
-import random
-import threading
-import time
+# So using a condition variable allows us to synchronize between two threads. But what if you had to synchronize across
+# multiple threads? A semaphore comes to the rescue. "A sema-what??" you ask.. :). Yeah that stuff was new to me as well
+# so I dug up a little. Apparently, semaphore was invented by a Dutch computer scientist - Edseger Dijkstra :(
+# Not only his inventions are pain, but so is his name. I believe keeping English as the world's communication language
+# till the foreseeable future, all the languages of the world including mine really ought to "normalize" the names that
+# are easily "proun-ciable" (if there is such a word!). I mean c'mmon... Edseger Dijkstra? As an Indian myself, I
+# pronounced his name as the sylables were introduced to me in kindergarten - "Ed-say-ger dee-jik-stra" but that doesn't
+# have a nice ring to it. So I googled :| It turns out, the name is pronounced "ets-kar daa-eek-stra" with a very
+# rounded D and T sounds (not sharp as in "dot" in english) :| Now, why does there have to be a j in his first name?
+# Well, only the "dutchman" knows, but the point is Mr. Dijkstra being genius and all managed to make our lives
+# miserable by investing his time into algorithms. This is a classic example of someone enjoying the cake and someone
+# else footing the bill :( ... Anyways, Dijkstra's famous "Shortest Path Algorithm" is well known in the field of
+# Computer Science. When I say "well known", I mean it is known to a lot of people, EXCEPT ME! But, depending on your
+# career aspirations or your desire to socialize with the "masters of the universe" within the company, you might choose
+# to know/study it. However, for the lesser mortals like me, as long as the "GPS lady" correctly guides you from
+# Sodawala Lane (Borivali) to Zhaveri Baazar (Kalbadevi), you are sorted.
+#
+# I have digressed... as I usually do. So, coming back, semaphore is a fancy word for "flag". This flag is basically a
+# signal used for communicating with another thread. Ever seen railway folks wave a physical flag to an incoming/
+# outgoing trains? THAT! my friend is a semaphore. If you google semaphore, that's what you will find in the images
+# section.
+#
+# In the current context, the semaphore acts as a signal to the other threads about a certain condition. Let's try to
+# understand this signalling by simulating a "parking lot problem" that we experience in everyday life. Again, this is
+# a very standard interview question should any of you attempt in the near future ;). So we know there are specific # of
+# slots in a parking lot. Any additional cars that come in will have to wait till somebody clears off their slot. That's
+# what we will attempt to build using semaphores. Let's see how.
+
+import uuid
+from concurrent.futures import ThreadPoolExecutor
 from threading import Semaphore
 
-
-class Employee(threading.Thread):
-    def __init__(self, name: str, zoom_number: threading.Semaphore) -> None:
-        super().__init__()
-        self.zoom_number = zoom_number
-        self.__name = name
-        self.__disconnect = False
-
-    def start(self) -> None:
-        delay = random.randint(1, 5)
-        time.sleep(delay)
-        self.zoom_number.acquire()
-        print(f'{self.__name} joined the call... [delay = {delay} seconds]')
-
-    def disconnect(self) -> None:
-        print(f'{self.__name} has disconnected')
-        time.sleep(1)
-        self.zoom_number.release()
+AVAILABLE_SLOTS = 5
 
 
-class Scrum(threading.Thread):
-    def __init__(self, name, zoom_number: threading.Semaphore):
-        super().__init__()
-        self._name = name
-        self.zoom_number = zoom_number
-        self.__disconnect = False
+class Car:
+    def __init__(self, employee: str) -> None:
+        self.name = employee
+        self._parked = False
+        self.token = None
 
-    def start(self) -> None:
-        self.zoom_number.acquire()
-        print(f'{self._name} has started...')
+    def assign_token(self, token):
+        self.token = token
+        self._parked = True if token else False
 
-    def end(self) -> None:
-        self.zoom_number.release()
-        print(f'{self._name} ended.')
+    def is_parked(self):
+        return self._parked
+
+
+class ParkingLot:
+    def __init__(self):
+        self.__slots: Semaphore = Semaphore(AVAILABLE_SLOTS)
+        self.__space: list = []
+
+    def park(self, car: Car) -> (bool, str, str):
+        if len(self.__space) < AVAILABLE_SLOTS:
+            self.__slots.acquire()
+            car.assign_token(uuid.uuid4())
+            self.__space.append(car)
+            return True, car.name, car.token
+        else:
+            return False, car.name, None
+
+    def unpark(self, token: str) -> Car | None:
+        if len(self.__space) > 0:
+            self.__slots.release()
+            slot = [s for s in self.__space if s.car.token == token]
+            car = slot[0]
+            car.assign_token(None)
+            return car
+        else:
+            return None
 
 
 def main() -> None:
-    # this will be our synchronizing mechanism.
-    zoom_number = Semaphore(7)
+    parking_lot = ParkingLot()
 
-    # let's create the participants of the call.
-    suhas = Employee('Suhas', zoom_number)
-    kavitak = Employee('Kavita K', zoom_number)
-    junaid = Employee('Junaid', zoom_number)
-    saif = Employee('Saif', zoom_number)
-    nitin = Employee('Nitin', zoom_number)
-    kavitay = Employee('Kavita Y', zoom_number)
+    cars = [Car('Suhas'), Car('KavitaK'), Car('KavitaY'), Car('Junaid'), Car('Saif'), Car('Avinash'),
+            Car('Dinesh'), Car('Nimisha'), Car('Abrar'), Car('Nitin'),
+            Car('Guru'), Car('Tejas'), Car('Purva'), Car('Chinmay'), Car('Mohita'), Car('Sana'),
+            Car('Purva'), Car('Sahana')]
 
-    # let's create the instance of a scrum call itself which needs to begin for people to start connecting.
-    scrum = Scrum('APAC scrum call', zoom_number)
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        result = executor.map(parking_lot.park, cars)
 
-    # and let's start the scrum call...
-    scrum.start()
-
-    #  We will now create a thread of each employee trying to join the call
-    suhas.start()
-    kavitak.start()
-    junaid.start()
-    saif.start()
-    nitin.start()
-    kavitay.start()
-
-    print("People are now discussing in the scrum call")
-    time.sleep(3)  # simulating the discussion
-    scrum.end()  # ending the call
-
-    # The threads will release the hold on the semaphore as well. That is tantamount to disconnecting from the call.
-    suhas.disconnect()
-    kavitak.disconnect()
-    junaid.disconnect()
-    saif.disconnect()
-    nitin.disconnect()
-    kavitay.disconnect()
-
-    print("Call is over.")
+    for r in result:
+        print(r)
 
 
 if __name__ == '__main__':
---
+++ threading/07_another_semaphore.py
@@ -0,0 +1,37 @@
+# importing the modules
+from threading import *
+import time
+
+# creating thread instance where count = 3
+my_obj = Semaphore(4)
+
+
+# creating instance
+def show(the_name):
+    # calling acquire method
+    my_obj.acquire()
+    for n in range(6):
+        print(f'Javatpoint, ')
+        time.sleep(1)
+        print(the_name)
+
+        # calling release method
+        my_obj.release()
+
+    # creating multiple thread
+
+
+thread_1 = Thread(target=show, args=('Thread 1',))
+thread_2 = Thread(target=show, args=('Thread 2',))
+thread_3 = Thread(target=show, args=('Thread 3',))
+thread_4 = Thread(target=show, args=('Thread 4',))
+thread_5 = Thread(target=show, args=('Thread 5',))
+thread_6 = Thread(target=show, args=('Thread 6',))
+
+# calling the threads
+thread_1.start()
+thread_2.start()
+thread_3.start()
+thread_4.start()
+thread_5.start()
+thread_6.start()
